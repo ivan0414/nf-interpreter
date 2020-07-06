26afbd30962731ec19f40a45017e21d9b0486d15
nfbot <nanoframework@outlook.com>
2020-07-06 09:14:27 +0000

    Code style fixes
    Automated fixes for code style.

--- src/CLR/Hardware.Esp32.Rmt/nanoFramework_hardware_esp32_rmt_native.h
+++ src/CLR/Hardware.Esp32.Rmt/nanoFramework_hardware_esp32_rmt_native.h
@@ -12,23 +12,22 @@
 
 struct Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtCommand
 {
-    static const int FIELD__level0 = 1;
-    static const int FIELD__level1 = 2;
+    static const int FIELD__level0     = 1;
+    static const int FIELD__level1     = 2;
     static const int FIELD__mDuration0 = 3;
     static const int FIELD__mDuration1 = 4;
 
     //--//
-
 };
 
 struct Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel
 {
-    static const int FIELD__commands = 1;
-    static const int FIELD___channel = 2;
-    static const int FIELD___carrierEnabled = 3;
+    static const int FIELD__commands             = 1;
+    static const int FIELD___channel             = 2;
+    static const int FIELD___carrierEnabled      = 3;
     static const int FIELD___carrierHighDuration = 4;
-    static const int FIELD___carrierLowDuration = 5;
-    static const int FIELD___carrierLevel = 6;
+    static const int FIELD___carrierLowDuration  = 5;
+    static const int FIELD___carrierLevel        = 6;
 
     NANOCLR_NATIVE_DECLARE(NativeInit___STATIC__I4__I4);
     NANOCLR_NATIVE_DECLARE(NativeGetIdleLevel___STATIC__BOOLEAN__I4);
@@ -45,9 +44,8 @@ struct Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Es
     NANOCLR_NATIVE_DECLARE(NativeSetSourceClock___STATIC__VOID__I4__BOOLEAN);
 
     //--//
-
 };
 
 extern const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_nanoFramework_Hardware_Esp32_Rmt;
 
-#endif  //_NANOFRAMEWORK_HARDWARE_ESP32_RMT_NATIVE_H_
+#endif //_NANOFRAMEWORK_HARDWARE_ESP32_RMT_NATIVE_H_
